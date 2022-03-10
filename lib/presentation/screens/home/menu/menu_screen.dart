861ee69cb8bf4bd1072fd814d4f9583bec2604fc
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2022-03-10 15:51:56 +0200

    updated menu screen

--- lib/presentation/screens/home/menu/menu_screen.dart
+++ lib/presentation/screens/home/menu/menu_screen.dart
@@ -1,108 +1,99 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0.0,
+    return ListView(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      shrinkWrap: true,
+      children: <Widget>[
+        ListTile(
+          title: Text(
+            'Favourites',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          leading: Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
         ),
-        body: Container(
-            color: Colors.white,
-            padding: const EdgeInsets.all(20),
-
-            child :ListView(
-              children: <Widget>[
-                ListTile(
-                  title: Text('Favourites',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  leading: Icon(Icons.favorite,
-                    color: Colors.red,),
-                ),
-                const Divider(
-                  height: 20,
-                  thickness: 0.5,
-                  indent: 20,
-                  endIndent: 0,
-                  color: Colors.grey,
-                ),
-                ListTile(
-                  title: Text('Visited Places',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  leading: Icon(Icons.location_on),
-                ),
-                const Divider(
-                  height: 20,
-                  thickness: 0.5,
-                  indent: 20,
-                  endIndent: 0,
-                  color: Colors.grey,
-                ),
-                ListTile(
-                  title: Text('Settings',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  leading: Icon(Icons.settings),
-                ),
-                const Divider(
-                  height: 20,
-                  thickness: 0.5,
-                  indent: 20,
-                  endIndent: 0,
-                  color: Colors.grey,
-                ),
-                ListTile(
-                  title: Text('About',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  leading: Icon(Icons.error_outline),
-                ),
-                const Divider(
-                  height: 25,
-                  thickness: 0.7,
-                  indent: 20,
-                  endIndent: 0,
-                  color: Colors.grey,
-                ),
-                ListTile(
-                  title: Text('Logout',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  leading: Icon(Icons.logout,
-                    color: Colors.blue,),
-                ),
-              ],)
-        ),),
+        const Divider(
+          height: 20,
+          thickness: 1,
+          color: Colors.grey,
+        ),
+        ListTile(
+          title: Text(
+            'Visited Places',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          leading: Icon(Icons.location_on),
+        ),
+        const Divider(
+          height: 20,
+          thickness: 1,
+          color: Colors.grey,
+        ),
+        ListTile(
+          title: Text(
+            'Settings',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          leading: Icon(Icons.settings),
+        ),
+        const Divider(
+          height: 20,
+          thickness: 1,
+          color: Colors.grey,
+        ),
+        ListTile(
+          title: Text(
+            'About',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          leading: Icon(Icons.error_outline),
+        ),
+        const Divider(
+          height: 20,
+          thickness: 1,
+          color: Colors.grey,
+        ),
+        ListTile(
+          title: Text(
+            'Logout',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          leading: Icon(
+            Icons.logout,
+            color: Colors.blue,
+          ),
+        ),
+      ],
     );
   }
 }
