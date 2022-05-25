f849f76b0f344529e05eaf295d82412228214296
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2022-05-26 00:41:42 +0200

    voiceover done

--- lib/presentation/screens/statue_info/voice_player.dart
+++ lib/presentation/screens/statue_info/voice_player.dart
@@ -1,13 +1,11 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 import 'package:flutter/material.dart';
 
 class VoicePlayer extends StatefulWidget {
-  const VoicePlayer({Key? key}) : super(key: key);
-
+  const VoicePlayer({Key? key, required this.url}) : super(key: key);
+  final String url;
   @override
   State<VoicePlayer> createState() => _VoicePlayerState();
 }
@@ -15,77 +13,72 @@ class VoicePlayer extends StatefulWidget {
 class _VoicePlayerState extends State<VoicePlayer> {
   bool _isPlaying = false;
   final AudioPlayer audioPlayer = AudioPlayer();
-  Duration duration=Duration.zero;
-  Duration position=Duration.zero;
-  static const url = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3';
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
   void seekToSec(int sec) {
     Duration newPos = Duration(seconds: sec);
-    audioPlayer.seek(newPos); // Jumps to the given position within the audio file
+    audioPlayer
+        .seek(newPos); // Jumps to the given position within the audio file
   }
-  void changeVolume(double value) {
-    audioPlayer.setVolume(value);
-  }
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    audioPlayer.onPlayerStateChanged.listen((state){
-      setState((){
-        _isPlaying=state==PlayerState.PLAYING;
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      setState(() {
+        _isPlaying = state == PlayerState.PLAYING;
       });
     });
-    audioPlayer.onDurationChanged.listen((newDuration){
-      setState((){
-        duration=newDuration;
+    audioPlayer.onDurationChanged.listen((newDuration) {
+      setState(() {
+        duration = newDuration;
       });
     });
 
-    audioPlayer.onAudioPositionChanged.listen((newPosition){
-      setState((){
-        position=newPosition;
-
+    audioPlayer.onAudioPositionChanged.listen((newPosition) {
+      setState(() {
+        position = newPosition;
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         _isPlaying
             ? IconButton(
-            onPressed: ()async{
-              if(_isPlaying){
-                await audioPlayer.pause();
-
-              } },
-
-            icon: Icon(
-              Icons.pause,
-              size: 35,
-              color: Theme.of(context).colorScheme.primary,
-            ))
+                onPressed: () async {
+                  if (_isPlaying) {
+                    await audioPlayer.pause();
+                  }
+                },
+                icon: Icon(
+                  Icons.pause,
+                  size: 35,
+                  color: Theme.of(context).colorScheme.primary,
+                ))
             : IconButton(
-          onPressed: ()async{
-            if(!_isPlaying){
-              audioPlayer.setVolume(9.6);
-              await audioPlayer.play(url);
-
-            } },
-
-          icon: Icon(
-            Icons.play_arrow,
-            size: 35,
-            color: Theme.of(context).colorScheme.primary,
-          ),
-        ),
+                onPressed: () async {
+                  if (!_isPlaying) {
+                    await audioPlayer.play(widget.url);
+                  }
+                },
+                icon: Icon(
+                  Icons.play_arrow,
+                  size: 35,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
         Expanded(
           child: Slider(
             min: 0,
             max: duration.inSeconds.toDouble(),
             value: position.inSeconds.toDouble(),
-            onChanged: (value)async{
-            seekToSec(value.toInt());
+            onChanged: (value) async {
+              seekToSec(value.toInt());
 
-            await audioPlayer.resume();
+              await audioPlayer.resume();
             },
           ),
         ),
