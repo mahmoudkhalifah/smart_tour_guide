2eabea2d22cfa5a89a39ddbf3651bb8c170e21da
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2022-10-22 16:22:10 +0200

    last edits before discussion

--- lib/strings.dart
+++ lib/strings.dart
@@ -4,6 +4,6 @@ const String forgotPasswordViewRoute = '/forget';
 const String homeViewRoute = '/home';
 const String placeInfoViewRoute = '/placeInfoView';
 const String statueInfoViewRoute = '/statueInfo';
-const String statuesViewRoute = '/statues';
-
+const 
+String statuesViewRoute = '/statues';
 const String baseUrl = "https://deployapiapp.azurewebsites.net/api/";
